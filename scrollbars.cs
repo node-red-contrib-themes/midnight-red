4cadd3f8607fcf8d7012bcb923f69d12c64b2dc2
Villhellm <wlatourelle@gmail.com>
2020-07-16 08:10:55 -0700

    Better dark theme scroll bars (#18)
    Adds more aesthetically pleasing scroll bars to fit the dark theme

---
+++ scrollbars.cs
@@ -0,0 +1,19 @@
+.red-ui-editor {
+  scrollbar-color: #999 #2E333A;
+}
+
+::-webkit-scrollbar {
+  width: 1em;
+}
+
+::-webkit-scrollbar-corner {
+  background-color: #2E333A;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #999;
+}
+
+::-webkit-scrollbar-track {
+  background-color: #2E333A;
+}
